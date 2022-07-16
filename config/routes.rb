dbde60814690a023e3e0ef6cc870c58923a42420
reisuta <in345vain@gmail.com>
2022-07-16 10:16:43 +0900

    book fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'aims#index'
   resources 'aims'
+  resources 'books'
   resources 'subscriptions'
   resources 'mobile_contracts'
   resources 'house_contracts'
   resources 'competitors'
-  resources 'books'
   resources 'categories'
 end
